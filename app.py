ae802d0b6c05a1e04d70bf34b3f23d8e36efa79a
Siddhant <siddhant99dei@gmail.com>
2021-10-24 14:37:00 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,49 @@
+# pip install streamlit
+import streamlit as st
+from model import predict_duration
+import numpy as np
+
+st.set_page_config(page_title="Seoul Bike Trip Duration Prediction App",
+                   page_icon="🛴", layout="wide")
+
+
+with st.form("prediction_form"):
+
+    st.header("Enter the significant values:")
+
+    distance = st.number_input("Distance: ", value=0, format="%d")
+    haversine = st.number_input("Haversine: ")
+    pmonth=st.slider("Pickup Month: ",  min_value=1, max_value=12)
+    pday=st.slider("Pickup Day: ",  min_value=1, max_value=31)
+    phour = st.slider("Pickup Hour: ", 0, 23, value=0, format="%d")
+    pmin = st.slider("Pickup Minute: ", 0, 59, value=0, format="%d")
+    pdweek=st.slider("Pickup Week: ",  min_value=0, max_value=6)
+    dmonth=st.slider("Drop Month: ",  min_value=1, max_value=12)
+    dday=st.slider("Drop Day: ",  min_value=1, max_value=31)
+    dhour = st.slider("Dropoff Hour: ", 0, 23, value=0, format="%d")
+    dmin = st.slider("Dropoff Minute: ", 0, 59, value=0, format="%d")
+    ddweek=st.slider("Drop Week: ",  min_value=0, max_value=6)
+    temp = st.number_input("Temp: ")
+    humid = st.number_input("Humid: ")
+    solar = st.number_input("Solar: ")
+    dust = st.number_input("Dust: ")
+
+    submit_val = st.form_submit_button("Predict Duration")
+
+if submit_val:
+    # If submit is pressed == True
+    attribute = np.array([distance, haversine,pmonth,pday, phour,
+                        pmin,pdweek,dmonth,dday, dhour,
+                        dmin,ddweek, temp,
+                        humid, solar, dust]).reshape(1,-1)
+
+
+    if attribute.shape == (1,16):
+        print("attributes valid")
+        
+
+        value = predict_duration(attributes= attribute)
+
+
+        st.header("Here are the results:")
+        st.success(f"The Duration predicted is {value} mins")
---
+++ model.py
@@ -0,0 +1,26 @@
+import joblib
+import matplotlib as plt
+import pandas as pd
+import numpy as np
+import os
+#from xgboost import XGBRegressor
+
+# Captures the path of current folder
+curr_path = os.path.dirname(os.path.realpath(__file__))
+
+
+feat_cols = ['Distance', 'Haversine', 'Pmonth', 'Pday', 'Phour', 'Pmin', 'PDweek',
+       'Dmonth', 'Dday', 'Dhour', 'Dmin', 'DDweek', 'Temp', 'Precip', 'Wind',
+       'Humid', 'Solar', 'Snow', 'GroundTemp', 'Dust']
+
+model= joblib.load(curr_path + "/model1.joblib")
+
+
+print(model)
+def predict_duration(attributes: np.ndarray):
+    """ Returns Bike Trip Duration value"""
+
+    pred = model.predict(attributes)
+    print("Duration predicted")
+
+    return int(pred[0])
